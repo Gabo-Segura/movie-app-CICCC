fa673fb69f6a92237db4997dd6fe80c53217d1a9
Mark Wong <markwong.digital@gmail.com>
2021-11-20 00:16:29 -0800

    Add dependencies for connecting REST api

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,18 @@
 module com.example.movieapp {
+    // for JavaFx
     requires javafx.controls;
     requires javafx.fxml;
 
+//    // for HTTP
+//    requires unirest.java;
+//
+//    // for JSON
+//    requires gson;
+//    requires java.sql;
 
+    // for JavaFx
     opens com.example.movieapp to javafx.fxml;
+
+    exports com.example.movieapp.components;
     exports com.example.movieapp;
 }
