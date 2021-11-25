164971043a669c6e4abe2e3739aa790f52befe3d
Mark Wong <markwong.digital@gmail.com>
2021-11-24 20:05:55 -0800

    Fix bugs

--- src/main/java/com/example/movieapp/controllers/DetailedMovieController.java
+++ src/main/java/com/example/movieapp/controllers/DetailedMovieController.java
@@ -8,6 +8,7 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import org.json.JSONObject;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,7 +19,7 @@ public class DetailedMovieController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        this.movie = new MovieResponse();
     }
 
     public void fetchMovie(int movieId) {
@@ -29,8 +30,14 @@ public class DetailedMovieController implements Initializable {
             HttpResponse<JsonNode> jsonRes =
                 Unirest.get(url).queryString("api_key", Config.API_KEY).asJson();
 
-            this.movie = MovieResponse.parse(jsonRes.getBody().getObject());
-            System.out.println(this.movie.toString());
+            JSONObject jsonObj = jsonRes.getBody().getObject();
+//            System.out.println(jsonObj);
+
+            this.movie = MovieResponse.parse(jsonObj);
+            System.out.println(this.movie);
+
+            // TODO: please write the method for displaying the movie detail herer
+
         } catch (UnirestException e) {
             e.printStackTrace();
         }
--- src/main/java/com/example/movieapp/models/movies/MovieResponse.java
+++ src/main/java/com/example/movieapp/models/movies/MovieResponse.java
@@ -25,6 +25,8 @@ public class MovieResponse {
     private boolean hasVideo;
     private double voteAverage;
 
+    public MovieResponse(){}
+
     public MovieResponse(int id, String posterPath, boolean isAdult, String overview, LocalDate releaseDate, List<Integer> genreIds, String originalTitle, String originalLanguage, String title, String backdropPath, double popularity, int voteCount, boolean hasVideo, double voteAverage) {
         this.id = id;
         this.posterPath = posterPath;
@@ -98,47 +100,102 @@ public class MovieResponse {
         return originalLanguage;
     }
 
-    public static MovieResponse parse(JSONObject json) {
+    public static MovieResponse parse(JSONObject json) throws JSONException {
         int id = json.getInt("id");
+
         String posterPath = "";
+        try {
+            posterPath = json.getString("poster_path");
+        } catch (JSONException e) {
+            System.out.println("missing poster path");
+        }
+
         boolean isAdult = false;
+        try {
+            isAdult = json.getBoolean("adult");
+        } catch (JSONException e) {
+            System.out.println("missing isAdult");
+        }
+
         String overview = "";
+        try {
+            overview = json.getString("overview");
+        } catch (JSONException e) {
+            System.out.println("missing overview");
+        }
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate releaseDate = null;
+        try {
+            releaseDate = LocalDate.parse(json.getString("release_date"), formatter);
+        } catch (JSONException e) {
+            System.out.println("missing release date");
+        }
 
         List<Integer> genreIds = new ArrayList<>();
-        String originalTitle = "";
-        String originalLanguage = "";
-        String title = "";
-        String backdropPath = "";
-        double popularity = 0.0;
-        int voteCount = 0;
-        boolean hasVideo = false;
-        double voteAverage = 0.0;
-
         try {
-            posterPath = json.getString("poster_path");
-            overview = json.getString("overview");
-            isAdult = json.getBoolean("adult");
-            releaseDate = LocalDate.parse(json.getString("release_date"), formatter);
-
-            // for genres
             JSONArray array = json.getJSONArray("genre_ids");
             for (Object o : array) {
                 genreIds.add((Integer) o);
             }
+        } catch (JSONException e) {
+            System.out.println("missing genre ids");
+        }
 
+        String originalTitle = "";
+        try {
             originalTitle = json.getString("original_title");
+        } catch (JSONException e) {
+            System.out.println("missing original title");
+        }
+
+        String originalLanguage = "";
+        try {
             originalLanguage = json.getString("original_language");
+        } catch (JSONException e) {
+            System.out.println("missing original language");
+        }
+
+        String title = "";
+        try {
             title = json.getString("title");
+        } catch (JSONException e) {
+            System.out.println("missing title");
+        }
+
+        String backdropPath = "";
+        try {
             backdropPath = json.getString("backdrop_path");
+        } catch (JSONException e) {
+            System.out.println("missing backdrop path");
+        }
+
+        double popularity = 0.0;
+        try {
             popularity = json.getDouble("popularity");
+        } catch (JSONException e) {
+            System.out.println("missing popularity");
+        }
+
+        int voteCount = 0;
+        try {
             voteCount = json.getInt("vote_count");
+        } catch (JSONException e) {
+            System.out.println("missing vote count");
+        }
+
+        boolean hasVideo = false;
+        try {
             hasVideo = json.getBoolean("video");
+        } catch (JSONException e) {
+            System.out.println("missing hasVideo");
+        }
+
+        double voteAverage = 0.0;
+        try {
             voteAverage = json.getDouble("vote_average");
         } catch (JSONException e) {
-            System.out.println("missing field...");
+            System.out.println("missing vote average");
         }
 
         return new MovieResponse(
