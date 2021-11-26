dfc15d3d39d1bc3532fc21977442288fda743a9c
Gabo-Segura <gabrielsegurao@me.com>
2021-11-26 15:32:01 -0500

    Small touches

--- src/main/java/com/example/movieapp/controllers/IndexController.java
+++ src/main/java/com/example/movieapp/controllers/IndexController.java
@@ -167,6 +167,7 @@ public class IndexController implements Initializable {
         Image img = new Image(imgUrl);
 
         this.heroPoster.setImage(img);
+        
     }
 
     private void setMoviesResponse(String type, JSONObject jsonObject) {
